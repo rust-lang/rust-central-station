797a0ca749a747dba6822cbfbd2bec0a184e6737
Alex Crichton <alex@alexcrichton.com>
2017-02-03 14:22:07 -0800

    Trim the output of `date`

--- promote-release/src/main.rs
+++ promote-release/src/main.rs
@@ -40,7 +40,7 @@ fn main() {
         release: env::args().nth(2).unwrap(),
         secrets: t!(secrets.parse()),
         handle: Easy::new(),
-        date: output(Command::new("date").arg("+%Y-%m-%d")),
+        date: output(Command::new("date").arg("+%Y-%m-%d")).trim().to_string(),
     }.run()
 }
 
