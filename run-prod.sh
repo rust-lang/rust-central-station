f2d521150bd5f4d9cb27b24b16bf22fb17050011
Alex Crichton <alex@alexcrichton.com>
2017-02-07 15:53:40 -0800

    Scope logging dir

--- run-prod.sh
+++ run-prod.sh
@@ -4,11 +4,12 @@ set -ex
 
 docker pull alexcrichton/rust-central-station
 
+mkdir -p /var/log/rcs/nginx
 exec docker run \
   --volume `pwd`:/src:ro \
   --volume `pwd`/data:/src/data \
   --volume `pwd`/data/letsencrypt:/etc/letsencrypt \
-  --volume /var/log:/var/log \
+  --volume /var/log/rcs:/var/log \
   --publish 80:80 \
   --publish 443:443 \
   --rm \
