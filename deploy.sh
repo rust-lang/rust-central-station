b48704df691f31b44640a18f11ce09853cd910c3
Alex Crichton <alex@alexcrichton.com>
2019-01-07 10:51:48 -0800

    Fail if anything else fails on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 $(aws ecr get-login --no-include-email --region us-west-1)
 docker tag rust-central-station:latest 890664054962.dkr.ecr.us-west-1.amazonaws.com/rust-central-station:latest
 docker push 890664054962.dkr.ecr.us-west-1.amazonaws.com/rust-central-station:latest
