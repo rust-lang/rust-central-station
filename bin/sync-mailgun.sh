d3dc91c44427bc42547f59c910b7ab3b6ee2b05b
Pietro Albini <pietro@pietroalbini.org>
2019-02-20 00:12:27 +0100

    make sync-mailgun.sh executable and show debug log for sync-mailgun

--- bin/sync-mailgun.sh
+++ bin/sync-mailgun.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 
 export MAILGUN_API_TOKEN="$(tq mailgun.token < /data/secrets.toml)"
+export RUST_LOG=mailgun_mailmap=debug
 exec run-on-change https://team-api.infra.rust-lang.org/v1/lists.json sync-mailgun
